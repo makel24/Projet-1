4d98826ffa2faeba2ba70796bce4603089e24fde
Mandy Kelly <makel24@ulaval.ca>
2023-11-13 17:39:15 -0500

    Fin de la phase1, analyse de la ligne de commande

--- phase1.py
+++ phase1.py
@@ -1,12 +1,13 @@
 import argparse
 import datetime
+from datetime import date
 
 
 def analyser_commande():
     parser = argparse.ArgumentParser(description="Extraction de valeurs historiques pour un ou plusieurs symboles boursiers")
-    parser.add_argument('-d', '--début', metavar= 'DATE', dest= 'début', type= ...help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
-    parser.add_argument('-f', '--fin', metavar= 'DATE', dest= 'fin', type= help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
-    parser.add_argument('-v', '--valeur', dest= 'valeur', choices= ['fermeture', 'ouverture', 'min', 'max', 'volume'], default= 'fermeture', help="La valeur désirée (par défaut: fermeture)")
+    parser.add_argument('-d', '--début', metavar= 'DATE', dest= 'début', type=datetime.date.fromisoformat, help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
+    parser.add_argument('-f', '--fin', metavar= 'DATE', dest= 'fin', default=date.today(), type=datetime.date.fromisoformat, help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
+    parser.add_argument('-v', '--valeur', dest= 'valeur', choices=['fermeture', 'ouverture', 'min', 'max', 'volume'], default= 'fermeture', help="La valeur désirée (par défaut: fermeture)")
     parser.add_argument('symbole', nargs= '+', metavar= 'symbole', help="Nom d'un symbole boursier")
     args = parser.parse_args()
     if args.début is None:
